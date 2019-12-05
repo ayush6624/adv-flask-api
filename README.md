92954558e87665287fc0ecbe4ae1a26e1272d92b
ayush6624 <ayushg1214@gmail.com>
2019-12-05 18:14:34 +0530

    Add: Main App

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Website - https://app.goyal.club
 # Objectives   
 
 ## Build a flask API 
-- [ ] A basic 2 page interface
+- [x] A basic 2 page interface
 - [ ] Simple interface to upload images and when you click "submit" button, it shows a new page with name of the uploaded image.
 - [ ] Build a key based authentication system using JWT token for accessing the api functionality.
 - [ ] Put in place a throttle for api call rate, let's say 5 / min.
--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if request.files:
+            upload_file = request.files['customFile']
+            file_name = upload_file.filename
+            print(request.headers['Content-Type'])
+            if request.headers['Content-Type'] == 'application/json':
+                return jsonify({"filename": file_name}, 201)
+            else:
+                return render_template('page.html', name=file_name), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not Found'}), 404
+
+
 if __name__ == "__main__":
-    app.run(debug="false", host='0.0.0.0', threaded="true")
+    app.run(debug="true", host='0.0.0.0', threaded="true")
