72ced3d8c2bca60a9a323fd06cfa53bdb98ee7ec
ayush6624 <ayushg1214@gmail.com>
2019-12-06 18:01:31 +0530

    Fixed Env Var Problem

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ limiter = Limiter(
     default_limits=["5 per minute"]
 )
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-# app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
-app.config['JWT_SECRET_KEY'] = 'ayushgoyal'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+app.config['JWT_SECRET_KEY'] = os.environ.get("SECRET")
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 mongo = pymongo.MongoClient(os.environ.get("MONGO_URI"))
 jwt = JWTManager(app)
@@ -53,6 +52,7 @@ def upload():
 
 
 @app.route('/user', methods=['GET', 'POST'])
+@jwt_required
 def user():
     if request.method == 'GET':
         query = request.args
